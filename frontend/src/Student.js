0a61218d890dc61c7dc9d08ccbbd9e2b7f19c8b9
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-28 16:19:22 +0545

    tried to fetch data from db

--- frontend/src/Student.js
+++ frontend/src/Student.js
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 export default function Student() {
+  const [student, setStudent] = useEffect([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:8081/")
-      .then((res) => console.log(res))
+      .then((res) => setStudent(res.data))
       .catch((err) => console.log(err));
   }, []);
 
@@ -16,11 +18,18 @@ export default function Student() {
         <table className="table">
           <thead>
             <tr>
-              <ht>Name</ht>
-              <ht>Email</ht>
+              <th>Name</th>
+              <th>Email</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {student.map((data, i) => (
+              <tr key={i}>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
