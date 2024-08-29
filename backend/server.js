059677143aaf76b04e0d0fd055fc44bcc466fd95
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-29 16:18:25 +0545

    created UpdateStudent component

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,22 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
-  const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?)";
+  const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?, ?)";
   const values = [req.body.name, req.body.email];
 
   db.query(sql, [values], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
+});
+
+app.put("/update/:id", (req, res) => {
+  const sql = "UPDATE student SET `Name` = ?, `Email` = ? WHERE ID = ?";
+  // const sql = "update student set Name = ? Email = ? where ID = ?";
+  const values = [req.body.name, req.body.email];
+  const id = req.params.id;
+
+  db.query(sql, [...values, id], (err, data) => {
     if (err) return res.json("Error");
     return res.json(data);
   });
