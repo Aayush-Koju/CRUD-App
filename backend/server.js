8287ef2f3bd9492d33fb8c096c059027feeccb78
Aayush-Koju <iush.cozu12@gmail.com>
2024-09-01 22:26:41 +0545

    added delete feature and fixed adding issue

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.post("/create", (req, res) => {
   const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?, ?)";
   const values = [req.body.name, req.body.email];
 
-  db.query(sql, [values], (err, data) => {
+  db.query(sql, values, (err, data) => {
     if (err) return res.json("Error");
     return res.json(data);
   });
@@ -43,11 +43,20 @@ app.post("/create", (req, res) => {
 
 app.put("/update/:id", (req, res) => {
   const sql = "UPDATE student SET `Name` = ?, `Email` = ? WHERE ID = ?";
-  // const sql = "update student set Name = ? Email = ? where ID = ?";
   const values = [req.body.name, req.body.email];
   const id = req.params.id;
 
   db.query(sql, [...values, id], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
+});
+
+app.delete("/student/:id", (req, res) => {
+  const sql = "DELETE FROM student WHERE ID = ?";
+  const id = req.params.id;
+
+  db.query(sql, [id], (err, data) => {
     if (err) return res.json("Error");
     return res.json(data);
   });
